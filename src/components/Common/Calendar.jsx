89ca244685516fb6f7eb3b4c844dd892f5e95dcb
qidian <qidian990107@163.com>
2020-11-07 12:39:18 -0800

    Added styled components in Calendar

--- src/components/Common/Calendar.jsx
+++ src/components/Common/Calendar.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import SmallCourseCard from './SmallCourseCard';
 import './Calendar.scss';
+import {
+  CalendarBody, CalendarContainer, CalendarHeader, CalendarHeaderItem, CalendarTimeline, CalendarTimelineItem,
+} from '../../styled';
 
 const times = [
   '8am',
@@ -20,84 +23,80 @@ const times = [
   '10pm',
 ];
 
-class Calendar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
- 
-  render() {
-    return (
-      <div className="calendar-main">
-        <div className="calendar-header">
-          <div className="time-column" />
-          <div className="header-item">Sun</div>
-          <div className="header-item">Mon</div>
-          <div className="header-item">Tues</div>
-          <div className="header-item">Wed</div>
-          <div className="header-item">Thurs</div>
-          <div className="header-item">Fri</div>
-          <div className="header-item">Sat</div>
-        </div>
-        <div className="scroll-container">
-          <div className="course-time-grid">
-            {times.map((time, index) => <div key={`course-time-${index}`}>{time}</div>)}
-          </div>
-          <div className="course-schedule-grid">
-            {times.map((_, index) => <div key={`course-grid-row-${index}`} />)}
-            <div className="course-cards-overlay">
-              <SmallCourseCard
-                datePrefix="2020-06-06T"
-                startTime="09:00"
-                endTime="09:50"
-                dayOfWeek={2}
-                type="LE"
-                title="CSE 120"
-                location="PCYNH 112"
-              />
-              <SmallCourseCard
-                datePrefix="2020-06-06T"
-                startTime="09:00"
-                endTime="09:50"
-                dayOfWeek={4}
-                type="LE"
-                title="CSE 121"
-                location="PCYNH 112"
-              />
-              <SmallCourseCard
-                datePrefix="2020-06-06T"
-                startTime="09:00"
-                endTime="09:50"
-                dayOfWeek={6}
-                type="LE"
-                title="CSE 122"
-                location="PCYNH 112"
-              />
-              <SmallCourseCard
-                datePrefix="2020-06-06T"
-                startTime="14:00"
-                endTime="15:20"
-                dayOfWeek={1}
-                type="LE"
-                title="COGS 10"
-                location="WLH 110"
-              />
-              <SmallCourseCard
-                datePrefix="2020-06-06T"
-                startTime="10:00:00"
-                endTime="10:50:00"
-                dayOfWeek={1}
-                type="DI"
-                title="CSE 123"
-                location="PCYNH 112"
-              />
-            </div>
-          </div>
+const Calendar = () => (
+  <CalendarContainer>
+    <CalendarHeader>
+      <CalendarHeaderItem time />
+      <CalendarHeaderItem>Sun</CalendarHeaderItem>
+      <CalendarHeaderItem>Mon</CalendarHeaderItem>
+      <CalendarHeaderItem>Tues</CalendarHeaderItem>
+      <CalendarHeaderItem>Wed</CalendarHeaderItem>
+      <CalendarHeaderItem>Thurs</CalendarHeaderItem>
+      <CalendarHeaderItem>Fri</CalendarHeaderItem>
+      <CalendarHeaderItem>Sat</CalendarHeaderItem>
+    </CalendarHeader>
+    <CalendarBody>
+      <CalendarTimeline>
+        {times.map((time, index) => (
+          <CalendarTimelineItem key={`course-time-${index}`}>
+            {time}
+          </CalendarTimelineItem>
+        ))}
+      </CalendarTimeline>
+      <div className="course-schedule-grid">
+        {times.map((_, index) => (
+          <div key={`course-grid-row-${index}`} />
+        ))}
+        <div className="course-cards-overlay">
+          <SmallCourseCard
+            datePrefix="2020-06-06T"
+            startTime="09:00"
+            endTime="09:50"
+            dayOfWeek={2}
+            type="LE"
+            title="CSE 120"
+            location="PCYNH 112"
+          />
+          <SmallCourseCard
+            datePrefix="2020-06-06T"
+            startTime="09:00"
+            endTime="09:50"
+            dayOfWeek={4}
+            type="LE"
+            title="CSE 121"
+            location="PCYNH 112"
+          />
+          <SmallCourseCard
+            datePrefix="2020-06-06T"
+            startTime="09:00"
+            endTime="09:50"
+            dayOfWeek={6}
+            type="LE"
+            title="CSE 122"
+            location="PCYNH 112"
+          />
+          <SmallCourseCard
+            datePrefix="2020-06-06T"
+            startTime="14:00"
+            endTime="15:20"
+            dayOfWeek={1}
+            type="LE"
+            title="COGS 10"
+            location="WLH 110"
+          />
+          <SmallCourseCard
+            datePrefix="2020-06-06T"
+            startTime="10:00:00"
+            endTime="10:50:00"
+            dayOfWeek={1}
+            type="DI"
+            title="CSE 123"
+            location="PCYNH 112"
+          />
         </div>
       </div>
-    );
-  }
-}
+    </CalendarBody>
+  </CalendarContainer>
+);
 
 export default Calendar;
