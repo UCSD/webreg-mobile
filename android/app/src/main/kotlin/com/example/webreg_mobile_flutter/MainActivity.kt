a5da9c4f057a9296c4323c0fc5f5ee2958674447
Wang, Danling <daw001@ucsd.edu>
2021-04-06 14:21:56 -0700

    set up flutter

---
+++ android/app/src/main/kotlin/com/example/webreg_mobile_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webreg_mobile_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- src/components/Common/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './Header';
-export { default as Calendar } from './Calendar';
-export { default as ListViewCard } from './ListViewCard';
-export { default as SearchResultCard } from './SearchResultCard';
-export { default as CourseDetailHeader } from './CourseDetailHeader';
-export { default as CourseDetailTableComponent } from './CourseDetailTableComponent';
